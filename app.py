bbbdd1f3208d1d241f752add8136b297277ab50f
jason <seekerjason@yahoo.com>
2025-05-14 10:34:26 +0800

    v4

--- app.py
+++ app.py
@@ -7,19 +7,19 @@ import os
 app=Flask(__name__)
 
 # google api is from https://makersuite.google.com
-# Run locally, uncomment the following line
+
+# Run locally based on the configuration file, uncomment the following two lines
 #app.config.from_pyfile("config.py", silent=True) 
-#config.py contains the secret_key which is the api_key of gemini
+#api_key=app.config['SECRET_KEY']
 
 # Run in colab, the uncomment the following two lines
 #from google.colab import userdata
-#api = userdata.get('makersuite')
+#api_key = userdata.get('makersuite')
 
 # Run in render.com, then uncomment the following line. MAKESUITE_API_KEY need to add into Enviornment Variables
-makersuite_api_key = os.getenv('MAKERSUITE_API_KEY')
+api_key = os.getenv('MAKERSUITE_API_KEY')
 
-
-genai.configure(api_key=app.config['SECRET_KEY'])
+genai.configure(api_key=api_key)
 model= genai.GenerativeModel("gemini-1.5-flash") #gemini-2.0-flash-001
 
 @app.route("/", methods=['GET', 'POST'])
